e13e8260369ee5acc363de49807cd74ddbe670ff
josevegara04 <jbvegar@eafit.edu.co>
2023-10-23 22:40:58 -0500

    Nueva propuesta

--- main.cpp
+++ main.cpp
@@ -6,116 +6,85 @@
 
 using namespace std;
 
-
-bool recorrer(vector<vector<int>> sudoku, int n, int num, int fila, int columna)
+// Funcion para imprimir el tablero
+void imprimir_sudoku(const vector<vector<int>>& sudoku) 
 {
-    int region = n;
-    int fila_region = (fila / region) * region;
-    int columna_region = (columna / region) * region;
-
-    for (int i = 0; i < pow(n, 2); i++) 
+    for (int i = 0; i < sudoku.size(); i++) 
     {
-        if (sudoku[i][columna] == num || sudoku[fila][i] == num) {
+        for (int j = 0; j < sudoku.size(); j++) 
+        {
+            cout << sudoku[i][j] << " ";
+        }
+        cout << std::endl;
+    }
+}
+
+// Funcion para verificar si un numero es valido en una celda
+bool es_numero_valido(const vector<vector<int>>& sudoku, int n, int fila, int columna, int numero) 
+{
+    // Verificar la fila
+    for (int i = 0; i < sudoku.size(); i++) 
+    {
+        if (sudoku[fila][i] == numero) 
+        {
             return false;
         }
     }
-    for (int i = fila_region; i < fila_region + region; i++) 
+
+    // Verificar la columna
+    for (int i = 0; i < sudoku.size(); i++) 
     {
-        for (int j = columna_region; j < columna_region + region; j++) 
+        if (sudoku[i][columna] == numero) 
         {
-            if (sudoku[i][j] == num) {
+            return false;
+        }
+    }
+
+    // Verificar la region
+    int inicioFila = (fila / n) * n;
+    int inicioColumna = (columna / n) * n;
+    for (int i = inicioFila; i < inicioFila + n; i++) 
+    {
+        for (int j = inicioColumna; j < inicioColumna + n; j++) 
+        {
+            if (sudoku[i][j] == numero) 
+            {
                 return false;
             }
         }
     }
+
     return true;
 }
 
-bool verificar_todo(vector<vector<int>>& sudoku) {
-    int n = pow(n, 2);  // Tamaño del sudoku (n x n)
-    int subregion = 2; // Tamaño de cada subregión
-
-    // Conjuntos para filas, columnas y subregiones
-    vector<set<int>> filas(n);
-    vector<set<int>> columnas(n);
-    vector<set<int>> subregiones(n);
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            int num = sudoku[i][j];
-
-            // Verificar si el número ya existe en la fila
-            if (filas[i].count(num) > 0) {
-                return false;
-            }
-            filas[i].insert(num);
-
-            // Verificar si el número ya existe en la columna
-            if (columnas[j].count(num) > 0) {
-                return false;
-            }
-            columnas[j].insert(num);
-
-            // Calcular la subregión a la que pertenece la celda
-            int sub_i = i / subregion;
-            int sub_j = j / subregion;
-            int sub_id = sub_i * subregion + sub_j;
-
-            // Verificar si el número ya existe en la subregión
-            if (subregiones[sub_id].count(num) > 0) {
-                return false;
-            }
-            subregiones[sub_id].insert(num);
-        }
-    }
-
-    // Si no se encontraron duplicados, el sudoku es válido
-    return true;
-}
-
-vector<vector<int>> resolver(vector<vector<int>> sudoku, int n, vector<int> numeros, int fila, int columna, int num_k, int* ultimo)
+// Funcion para resolver el Sudoku 
+bool resolver_sudoku(vector<vector<int>>& sudoku, int n, vector<int>& numeros) 
 {
-    if(verificar_todo(sudoku))
+    for (int fila = 0; fila < sudoku.size(); fila++) 
     {
-        return sudoku;
-    }
-
-    int num = sudoku[fila][columna];
-    if(num == 0)
-    { 
-        for(int k = num_k; k < numeros.size(); k++)
+        for (int columna = 0; columna < sudoku.size(); columna++) 
         {
-            sudoku[fila][columna] = numeros[k];
-            if(recorrer(sudoku, n, numeros[k], fila, columna))
-            {
-                for(int i = 0; i < pow(n, 2); i++)
+            if (sudoku[fila][columna] == 0) 
+            { 
+                for (int numero = 1; numero <= numeros.size(); numero++) 
                 {
-                    for(int j = 0; j < pow(n, 2); j++)
+                    if (es_numero_valido(sudoku, n, fila, columna, numero)) 
                     {
-                        cout << sudoku[i][j] << "|";
+                        sudoku[fila][columna] = numero;  
+
+                        if (resolver_sudoku(sudoku, n, numeros)) 
+                        {  
+                            return true;
+                        }
+
+                        sudoku[fila][columna] = 0;  
                     }
-                    cout << endl;
-                    cout << "---------------------";
-                    cout << endl;
                 }
-                ultimo[0] = fila;
-                ultimo[1] = columna;
-                if(columna == pow(n, 2) - 1)
-                {
-                    sudoku = resolver(sudoku, n, numeros, fila + 1, 0, 0, ultimo);
-                }
-                else
-                {
-                    sudoku = resolver(sudoku, n, numeros, fila, columna + 1, 0, ultimo);
-                }
+                return false;  
             }
-            else
-            {
-                sudoku[fila][columna] = 0;
-            }
-        } 
+        }
     }
-    return sudoku;
+    return true;  
 }
 
 int main()
@@ -135,19 +104,18 @@ int main()
 
         vector<vector<int>> sudoku(pow(n, 2), vector<int>(pow(n, 2)));
         vector<int> numeros;
-        for(int i = 1; i <= pow(n, 2); i++)
+        for(int i = 1; i <= sudoku.size(); i++)
         {
             numeros.push_back(i);
         }
 
-        for(int i = 0; i < pow(n, 2); i++)
+        for(int i = 0; i < sudoku.size(); i++)
         {
             getline(file, line);
             vector<string> subcadenas;
             for (int j = 0; j < line.length(); j += 2) 
             {
                 string subcadena = line.substr(j, symbol);
-                /* cout << subcadena << endl; */
 
                 if (subcadena == "--") {
                     subcadenas.push_back("0");
@@ -156,28 +124,21 @@ int main()
                 }
             }
 
-            for (int k = 0; k < subcadenas.size(); k++) 
+            for (int j = 0; j < subcadenas.size(); j++) 
             {
-                int num = stoi(subcadenas[k]);
-                sudoku[i][k] = num;
-                /* cout << sudoku[i][k] << endl; */
+                int num = stoi(subcadenas[j]);
+                sudoku[i][j] = num;
             }
         }
         file.close();
 
-    
-
-        sudoku = resolver(sudoku, n, numeros, 0, 0, 0, new int[2]);
-
-        for(int i = 0; i < pow(n, 2); i++)
+        if(resolver_sudoku(sudoku, n, numeros))
         {
-            for(int j = 0; j < pow(n, 2); j++)
-            {
-                cout << sudoku[i][j] << "|";
-            }
-            cout << endl;
-            cout << "---------------------";
-            cout << endl;
+            imprimir_sudoku(sudoku);
+        }
+        else
+        {
+            cout << "No se pudo resolver el sudoku" << endl;
         }
         
     }
--- prueba.cpp
+++ prueba.cpp
@@ -1,240 +1,109 @@
 #include <iostream>
 #include <vector>
-#include <set> 
-#include <cmath>
 
-using namespace std;
-
-bool recorrer(vector<vector<int>> sudoku, int n, int num, int fila, int columna)
+// Función para imprimir el tablero del Sudoku
+void imprimirSudoku(std::vector<std::vector<int>>& sudoku) 
 {
-    cout << "Region: " << n << ", Numero: " << num << ", Fila: " << fila << ", Columna: " << columna << endl;
-    set<int> verificados;
-
-    //loop para revisar que el numero no este repetido en la fila
-    cout << endl << "Verificando fila..." << endl;
-	for(int i = 0; i < sudoku.size(); i++)
+    for (int i = 0; i < 9; i++) 
     {
-        if(sudoku[fila][sudoku.size() - 1] == num)
+        for (int j = 0; j < 9; j++) 
+        {
+            std::cout << sudoku[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+// Función para verificar si un número es válido en una celda
+bool esNumeroValido(const std::vector<std::vector<int>>& sudoku, int fila, int columna, int numero) 
+{
+    // Verificar la fila
+    for (int i = 0; i < 9; i++) 
+    {
+        if (sudoku[fila][i] == numero) 
         {
             return false;
         }
-        if(sudoku[fila][i] == num)
-        {
-            if(verificados.count(num) > 0)
-            {
-                return false;
-            }
-            verificados.insert(num);
-        }
     }
-    verificados.clear();
 
-    //loop para revisar que el numero no este repetido en la columna
-    cout << endl << "Verificando columna..." << endl;
-	for(int i = 0; i < sudoku.size(); i++)
+    // Verificar la columna
+    for (int i = 0; i < 9; i++) 
     {
-        if(sudoku[sudoku.size() - 1][columna] == num)
+        if (sudoku[i][columna] == numero) 
         {
             return false;
         }
-        if(sudoku[i][columna] == num)
+    }
+
+    // Verificar el cuadro 3x3
+    int inicioFila = (fila / 3) * 3;
+    int inicioColumna = (columna / 3) * 3;
+    for (int i = inicioFila; i < inicioFila + 3; i++) 
+    {
+        for (int j = inicioColumna; j < inicioColumna + 3; j++) 
         {
-            if(verificados.count(num) > 0)
+            if (sudoku[i][j] == numero) 
             {
                 return false;
             }
-            verificados.insert(num);
         }
     }
-    verificados.clear();
 
-    //loop para revisar que el numero no este repetido en la region 
-    cout << endl << "Verificando region..." << endl;
-    int fila_region = (fila / n) * n;
-    int columna_region = (columna / n) * n;
-    cout << "Empieza desde la fila: " << fila_region << " y la columna: " << columna_region << endl;
-
-    for(int i = fila_region; i < fila_region + n; fila_region++)
-    {
-        for(int j = columna_region; j < columna_region + n; columna_region++)
-        {
-            if(sudoku[n - 1][n - 1] == num)
-            {
-                return false; 
-            }
-            if(sudoku[i][j] == num)
-            {
-                if(verificados.count(num) > 0)
-                {
-                    return false;
-                }
-                verificados.insert(num);
-            }
-        }
-    }
     return true;
 }
 
-bool verificar_todo(vector<vector<int>> sudoku, int n) 
+// Función para resolver el Sudoku recursivamente
+bool resolverSudoku(std::vector<std::vector<int>>& sudoku) 
 {
-    set<int> verificados;
-
-    //Se revisa que no haya ningun espacio en blanco
-    for(int i = 0; i < sudoku.size(); i++)
+    for (int fila = 0; fila < 9; fila++) 
     {
-        for(int j = 0; j < sudoku.size(); j++)
+        for (int columna = 0; columna < 9; columna++) 
         {
-            if(sudoku[i][j] == 0)
-            {
-                return false;
-            }
-        }
-    }
-
-    //se verifican que los numeros no se repitan en las columnas
-    for(int i = 0; i < sudoku.size(); i++)
-    {
-        for(int j = 0; j < sudoku.size(); j++)
-        {
-            verificados.insert(sudoku[j][i]);
-        }
-        for(int j = 0; j < verificados.size(); j++)
-        {
-            if(verificados.count(j) > 1)
-            {
-                return false;
-            }
-        }
-        verificados.clear();
-    }
-
-    //Se verifica que los numeros no se repitan en las filas
-    for(int i = 0; i < sudoku.size(); i++)
-    {
-        for(int j = 0; j < sudoku.size(); j++)
-        {
-            verificados.insert(sudoku[i][j]);
-        }
-        for(int j = 0; j < verificados.size(); j++)
-        {
-            if(verificados.count(j) > 1)
-            {
-                return false;
-            }
-        }
-        verificados.clear();
-    }
-
-    //Se verifica que en cada region, no se repita ningun numero
-    for(int fila_region = 0; fila_region < sudoku.size(); fila_region += n)
-    {
-        for(int columna_region = 0; columna_region < sudoku.size(); columna_region += n)
-        {
-            for(int i = fila_region; i < fila_region + n; fila_region++)
-            {
-                for(int j = columna_region; j < columna_region + n; columna_region++)
+            if (sudoku[fila][columna] == 0) 
+            {  // Celda vacía
+                for (int numero = 1; numero <= 9; numero++) 
                 {
-                    verificados.insert(sudoku[i][j]);
+                    if (esNumeroValido(sudoku, fila, columna, numero)) 
+                    {
+                        sudoku[fila][columna] = numero;  // Intenta un número
+
+                        if (resolverSudoku(sudoku)) 
+                        {  // Llama recursivamente
+                            return true;
+                        }
+
+                        sudoku[fila][columna] = 0;  // Si no es válido, deshacer
+                    }
                 }
+                return false;  // No se puede resolver
             }
-            for(int k = 0; k < verificados.size(); k++)
-            {
-                if(verificados.count(k) > 1)
-                {
-                    return false;
-                }
-            }
-            verificados.clear();
         }
     }
-    return true;
+    return true;  // Sudoku resuelto
 }
 
-void resolver(vector<vector<int>> sudoku, int n, vector<int> numeros, int fila, int columna, int num_k, int ultimo_i, int ultimo_j)
-{
-	cout << "Resolviendo..." << endl;
-    if(verificar_todo(sudoku, n))
+int main() {
+    std::vector<std::vector<int>> sudoku = 
     {
-    	cout << "¡Sudoku resuelto!" << endl;
-        return;
-    }
-
-	cout << "No esta bien" << endl;
-    cout << "Fila: " << fila << ", Columna: " << columna << " y el numero es: " << sudoku[fila][columna] << endl;
-    if(sudoku[fila][columna] == 0)
-    { 
-        for(int k = num_k; k < numeros.size(); k++)
-        {
-            sudoku[fila][columna] = numeros[k];
-            cout << "Verificando si el numero " << numeros[k] << " se puede poner..." << endl;
-            if(recorrer(sudoku, n, sudoku[fila][columna], fila, columna))
-            {
-            	cout << "El numero cambio a: " << sudoku[fila][columna] << endl;
-            	ultimo_i = fila;
-                ultimo_j = columna;
-                
-                if(columna == sudoku.size() - 1)
-                {
-                    resolver(sudoku, n, numeros, fila + 1, 0, 1, ultimo_i, ultimo_j);
-                }
-                else
-                {
-                    resolver(sudoku, n, numeros, fila, columna + 1, 1, ultimo_i, ultimo_j);
-                }
-            }
-            else
-            {
-            	cout << sudoku[fila][columna] << " Numero invalido " << endl;
-                sudoku[fila][columna] = 0;
-            }
-        } 
-    }
-    else
-    {
-    	cout << "El numero no esta en blanco" << endl;
-    	resolver(sudoku, n, numeros, fila, columna + 1, 0, ultimo_i, ultimo_j);
-        
-    }
-}
-
-
-int main()
-{
-    vector<vector<int>> sudoku = 
-    {
-        {6,0,1,0,0,4,0,8,0},
-        {0,4,9,7,3,0,0,0,0},
-        {8,2,3,0,0,0,0,4,9},
-        {0,0,4,0,0,0,0,9,6},
-        {0,0,7,0,0,0,2,0,0},
-        {9,8,0,0,0,0,3,0,0},
-        {4,1,0,0,0,0,9,6,5},
-        {0,0,0,0,4,1,8,3,0},
-        {0,5,0,2,0,0,4,0,7}
+        {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}
     };
-    
-    int n = 3;
-    
-    vector<int> numeros;
-    for(int i = 1; i <= pow(n, 2); i++)
-    {
-        numeros.push_back(i);
-    }
-        
-    
-    int ultimo_i = 0;
-    int ultimo_j = 0;
-    resolver(sudoku, n, numeros, 0, 0, 0, ultimo_i, ultimo_j);
 
-    for(int i = 0; i < pow(n, 2); i++)
+    if (resolverSudoku(sudoku)) 
     {
-        for(int j = 0; j < pow(n, 2); j++)
-        {
-            cout << sudoku[i][j] << "|";
-        }
-        cout << endl;
-        cout << "---------------------";
-        cout << endl;
+        std::cout << "Sudoku resuelto:" << std::endl;
+        imprimirSudoku(sudoku);
+    } else 
+    {
+        std::cout << "No se encontró solución para el Sudoku." << std::endl;
     }
+
     return 0;
 }
