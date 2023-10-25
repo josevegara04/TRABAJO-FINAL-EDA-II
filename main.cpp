ccf8a4bb6a6ea6e7e4bf4fd37dfbf3b0dfa76f4c
josevegara04 <jbvegar@eafit.edu.co>
2023-10-24 19:53:28 -0500

    Se añadió el archivo resolver_sudoku.cpp y validar_sudoku.cpp

--- main.cpp
+++ main.cpp
@@ -2,90 +2,10 @@
 #include <fstream>
 #include <cmath>
 #include <vector>
-#include <set>
+#include "resolver_sudoku.cpp"
+#include "validar_sudoku.cpp"
 
 using namespace std;
-
-// Funcion para imprimir el tablero
-void imprimir_sudoku(const vector<vector<int>>& sudoku) 
-{
-    for (int i = 0; i < sudoku.size(); i++) 
-    {
-        for (int j = 0; j < sudoku.size(); j++) 
-        {
-            cout << sudoku[i][j] << " ";
-        }
-        cout << std::endl;
-    }
-}
-
-// Funcion para verificar si un numero es valido en una celda
-bool es_numero_valido(const vector<vector<int>>& sudoku, int n, int fila, int columna, int numero) 
-{
-    // Verificar la fila
-    for (int i = 0; i < sudoku.size(); i++) 
-    {
-        if (sudoku[fila][i] == numero) 
-        {
-            return false;
-        }
-    }
-
-    // Verificar la columna
-    for (int i = 0; i < sudoku.size(); i++) 
-    {
-        if (sudoku[i][columna] == numero) 
-        {
-            return false;
-        }
-    }
-
-    // Verificar la region
-    int inicioFila = (fila / n) * n;
-    int inicioColumna = (columna / n) * n;
-    for (int i = inicioFila; i < inicioFila + n; i++) 
-    {
-        for (int j = inicioColumna; j < inicioColumna + n; j++) 
-        {
-            if (sudoku[i][j] == numero) 
-            {
-                return false;
-            }
-        }
-    }
-
-    return true;
-}
-
-// Funcion para resolver el Sudoku 
-bool resolver_sudoku(vector<vector<int>>& sudoku, int n, vector<int>& numeros) 
-{
-    for (int fila = 0; fila < sudoku.size(); fila++) 
-    {
-        for (int columna = 0; columna < sudoku.size(); columna++) 
-        {
-            if (sudoku[fila][columna] == 0) 
-            { 
-                for (int numero = 1; numero <= numeros.size(); numero++) 
-                {
-                    if (es_numero_valido(sudoku, n, fila, columna, numero)) 
-                    {
-                        sudoku[fila][columna] = numero;  
-
-                        if (resolver_sudoku(sudoku, n, numeros)) 
-                        {  
-                            return true;
-                        }
-
-                        sudoku[fila][columna] = 0;  
-                    }
-                }
-                return false;  
-            }
-        }
-    }
-    return true;  
-}
 
 int main()
 {
@@ -134,7 +54,7 @@ int main()
 
         if(resolver_sudoku(sudoku, n, numeros))
         {
-            imprimir_sudoku(sudoku);
+            imprimir_sudoku(sudoku, n);
         }
         else
         {
---
+++ validar_sudoku.cpp
@@ -0,0 +1,60 @@
+#include <iostream> 
+#include <set> 
+#include <vector> 
+#include <cmath>
+
+using namespace std;
+
+bool validar_sudoku(const vector<vector<int>>& sudoku, int n, vector<int> numeros)
+{
+    set<int> verificados;
+
+    //Verificar filas
+    for(int i = 0; i < sudoku.size(); i++)
+    {
+        for(int j = 0; j < sudoku.size(); j++)
+        {
+            if(sudoku[i][j] == 0 || verificados.count(sudoku[i][j]) > 0)
+            {
+                return false;
+            }
+            verificados.insert(sudoku[i][j]);
+        }
+        verificados.clear();
+    }
+
+    //Verificar columnas
+    for(int i = 0; i < sudoku.size(); i++)
+    {
+        for(int j = 0; j < sudoku.size(); j++)
+        {
+            if(sudoku[j][i] == 0 || verificados.count(sudoku[j][i]) > 0)
+            {
+                return false;
+            }
+            verificados.insert(sudoku[j][i]);
+        }
+        verificados.clear();
+    }
+    
+    //Verificar regiones
+    for(int fila_region = 0; fila_region < sudoku.size(); fila_region += n)
+    {
+        for(int columna_region = 0; columna_region < sudoku.size(); columna_region += n)
+        {
+            for(int fila = fila_region; fila < fila_region + n; fila++)
+            {
+                for(int columna = columna_region; columna < columna_region + n; columna++)
+                {
+                    if(sudoku[fila][columna] == 0 || verificados.count(sudoku[fila][columna]) > 0)
+                    {
+                        return false;
+                    }
+                    verificados.insert(sudoku[fila][columna]);
+                }
+            }
+            verificados.clear();
+        }
+    }
+    return true;
+}
